d1b7747b0364238f86f0ae676c69e55c11b83563
Vinh <vinhpg45@gmail.com>
2019-03-17 21:38:38 +0000

    added results from TPOT during 3.5 days

--- outputs/coords_y_tpot.py
+++ outputs/coords_y_tpot.py
@@ -1,7 +1,11 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.pipeline import make_pipeline, make_union
+from tpot.builtins import StackingEstimator
+from xgboost import XGBRegressor
+from sklearn.preprocessing import FunctionTransformer
+from copy import copy
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,8 +13,14 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Average CV score on the training set was:0.8097099106385609
-exported_pipeline = DecisionTreeClassifier(criterion="gini", max_depth=7, min_samples_leaf=11, min_samples_split=12)
+# Average CV score on the training set was:-148.4323395712502
+exported_pipeline = make_pipeline(
+    make_union(
+        FunctionTransformer(copy),
+        FunctionTransformer(copy)
+    ),
+    XGBRegressor(learning_rate=0.1, max_depth=9, min_child_weight=19, n_estimators=100, nthread=1, subsample=0.9500000000000001)
+)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
--- outputs/player_tpot.py
+++ outputs/player_tpot.py
@@ -1,11 +1,12 @@
 import numpy as np
 import pandas as pd
+from sklearn.decomposition import FastICA
 from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
-from sklearn.tree import DecisionTreeClassifier
-from tpot.builtins import StackingEstimator
+from tpot.builtins import StackingEstimator, ZeroCount
+from sklearn.preprocessing import FunctionTransformer
+from copy import copy
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -13,12 +14,16 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Average CV score on the training set was:0.8153934194716387
+# Average CV score on the training set was:0.1803184706385769
 exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=DecisionTreeClassifier(criterion="entropy", max_depth=4, min_samples_leaf=10, min_samples_split=5)),
-    StackingEstimator(estimator=LogisticRegression(C=1.0, dual=False, penalty="l2")),
-    StackingEstimator(estimator=DecisionTreeClassifier(criterion="entropy", max_depth=4, min_samples_leaf=17, min_samples_split=5)),
-    ExtraTreesClassifier(bootstrap=True, criterion="entropy", max_features=0.7500000000000001, min_samples_leaf=12, min_samples_split=12, n_estimators=100)
+    make_union(
+        make_pipeline(
+            FastICA(tol=0.4),
+            ZeroCount()
+        ),
+        FunctionTransformer(copy)
+    ),
+    ExtraTreesClassifier(bootstrap=False, criterion="gini", max_features=0.15000000000000002, min_samples_leaf=1, min_samples_split=2, n_estimators=100)
 )
 
 exported_pipeline.fit(training_features, training_target)
