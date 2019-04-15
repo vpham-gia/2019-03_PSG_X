f698ed441a433ff5a181b9f1de72c2405f86fe1d
Vinh <vinhpg@free.fr>
2019-04-15 22:13:31 +0200

    feat: modified install_psgx.py

--- init.sh
+++ init.sh
@@ -1,18 +1,22 @@
 echo "Starting project initialization for Ubuntu."
+echo ""
 
 echo "Updating apt-get and installing packages .."
+echo ""
 sudo apt-get update
 sudo apt-get install build-essential checkinstall wget
 sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
 cd /usr/src
 
 echo "Installing python 3.5.6 .."
+echo ""
 sudo wget https://www.python.org/ftp/python/3.5.6/Python-3.5.6.tgz
 sudo tar xzf Python-3.5.6.tgz
 cd Python-3.5.6
 sudo ./configure --enable-optimizations
 sudo make install
 
+echo ""
 echo "Done - environment is ready"
 
 # echo "Installing pipenv .."
